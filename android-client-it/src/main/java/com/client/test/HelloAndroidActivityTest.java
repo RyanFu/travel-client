0b3df5f9a3b7f5a29dceb2211a5cad98fb4460df
Anuj Jamwal <anujjamwal@thoughtworks.com>
2012-09-04 14:27:47 +0530

    Basic setup Maven android

---
+++ android-client-it/src/main/java/com/client/test/HelloAndroidActivityTest.java
@@ -0,0 +1,18 @@
+package com.client.test;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.client.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
